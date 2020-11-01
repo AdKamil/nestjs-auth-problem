f96ea8eca79d90318e430a8bed4cbaeb7fe4f952
AdKamil <kamil.grzaba@gmail.com>
2020-11-01 20:55:16 +0100

    nest-cli added to dockerfile, auth and users modules created

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TestModule } from './TestModule/test.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(`mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`),
-    TestModule
+    TestModule,
+    AuthModule,
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
